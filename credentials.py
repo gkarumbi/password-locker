cdea978400e2c40fa7536928d4e186602a9b22cd
gkarumbi <gkarumbi@gmail.com>
2020-06-29 20:36:24 +0300

    Import users into our credential class and create and instance using the setUp function

---
+++ credentials.py
@@ -0,0 +1,13 @@
+from user import user
+
+class Credentials:
+    """
+    A Class that generates credentials
+    """
+
+    def setUp(self):
+        self.new_user = User("gkarumbi","karumbi007")
+
+
+if __name__ = "__main__":
+    main()
