395302f629125e2c565e83eaafad450c49b76924
gkarumbi <gkarumbi@gmail.com>
2020-06-29 20:46:13 +0300

    Add method to create password

--- credentials.py
+++ credentials.py
@@ -5,9 +5,17 @@ class Credentials:
     A Class that generates credentials
     """
 
+    password_list = []
+
     def setUp(self):
         self.new_user = User("gkarumbi","karumbi007")
+
+    
 
 
 if __name__ = "__main__":
     main()
+
+    def createPassword(password):
+
+        return password
