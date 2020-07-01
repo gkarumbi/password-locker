b70416718c7b260ae3c80d232a7622b207618af0
gkarumbi <gkarumbi@gmail.com>
2020-07-01 10:13:32 +0300

    Add logic to take in user input manipulate it and display user output

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from user import User
 This where we shall be running our app
 """
 
-def createUserAccount():
+def createUserAccount(user_name,email,password):
     """
     Create new account
     """
@@ -57,12 +57,68 @@ def main():
 
     name = input()
 
-    print(name +" Please use the following short codes: 'ca' to create account, 'si' to sign in and 'x' to exit" )
+    print(name +" Please use the following short codes: 'ca' to create account, 'si' to sign in, 'sa' to search accounts, 'ds' to display accounts  and 'x' to exit" )
 
     while True:
 
         input_code = input().lower()
-        
+
+        if input_code == "ca":
+            print("Create a new account by keying in your account details")
+            print("Key in your username")
+            user_name = input()
+
+            print('\n')
+            print("Key in your email")
+            email = input()
+
+            print("Key in your password")
+
+            password = input()
+
+            save_new_user(createUserAccount(user_name,email,password))
+
+            print("See your account details below")
+            print(f"Username: {user_name},email: {email},password:{password}")
+
+            print('\n')
+
+            print(f"Welcome {user_name}, you have been succesfully logged in")
+        elif input_code == 'si'
+            print("Enter your account details")
+            print("Account Name")
+            account = input()
+
+            print("Email")
+
+            email  = input()
+
+            print("Password")
+
+            password = input()
+
+            save_user_credentials(createCredentials(account,email,password))
+
+        elif input_code == "ds":
+            print(" Your account credentials are as follows}:")
+            print ('\n')
+            for creds in show_credentials():
+                    print(f"{creds.account}\n {creds.email}\n {creds.password}")
+            else: 
+                print ('\n')
+                print("Sorry, You do not have any accounts saved")
+
+        elif short_code == "sa":
+                print("Key in the name of the account you are looking for: ")
+                userCred_input = input()
+                if search_by_account(userCred_input):
+                    account_search = search_by_account(userCred_inputserCred)
+                    print(f"{account_search.account} {account_search.email} {account_search.password}")
+                else: print("Sorry! The account does not exist!")
+        elif input_code =="ex"
+            print("Logging out..")
+            break
+
 
 
     if __name__ == "__main__":
